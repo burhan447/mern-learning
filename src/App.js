8fb08bb927ad5b0a5ac389f7a04bd71b923f6c6e
hussaingolwaala <golwalahussain3@gmail.com>
2023-02-15 15:56:15 +0530

    code formatted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Dashboard from './components/Dashboard/Dashboard'
-import Login from './components/Login/Login'
-import Signup from './components/Signup/Signup'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path='/signup' element={ <Signup /> } />
-      <Route path='/dashboard' element={ <Dashboard /> } />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
